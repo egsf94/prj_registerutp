39c3266193230b4b8cd12046198e97158dba642a
eddinson <egsf_94@outlook.com>
2023-02-13 16:36:18 -0500

    conexion successfully and add pdoconfig

--- pdo.php
+++ pdo.php
@@ -1,9 +1,13 @@
 <?php
-try{
-    $conexion = new PDO('mysql:host=192.168.0.8;dbname=dbregutp','conec','Base2023$');
-    echo "Conexion OK";
-}catch(PDOExcepction $e){
-    echo "Error: " . $e->getMessage();
+require_once 'pdoconfig.php'; 
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;charset=utf8;dbname=$dbname", $username, $password);
+    //echo "Connected to $dbname at $host successfully.";
+    //foreach($conn->query('SELECT * from materia') as $fila) {
+    //    print_r($fila);
+    //}
+    //$conn = null;
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
-
 ?>
---
+++ pdoconfig.php
@@ -0,0 +1,7 @@
+<?php
+$host = "192.168.0.8";
+$dbname = "dbregutp";
+$username = "conec";
+$password = "Base2023";
+$port=3306;
+?>
